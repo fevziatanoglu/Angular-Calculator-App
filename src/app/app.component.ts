8869a2dacc0c3f650a7a898b154c4b64f8e1bb94
Fevzi Atanoğlu <fevziatan2@gmail.com>
2023-07-08 18:05:43 +0300

    Theme Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Calculator-App';
+
+  darkTheme = false;
+
+  themeToggle(){
+    this.darkTheme = !this.darkTheme;
+
+   document.documentElement.setAttribute('data-theme', this.darkTheme ? "dark" : "light");
+
+   console.log("dark");
+  }
 }
